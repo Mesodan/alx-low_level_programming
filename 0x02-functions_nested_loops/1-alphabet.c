014c02a8dff99ee1df1d7bc423a8450826c1d529
Mesodan <dansakin@gmail.com>
2023-02-21 12:26:23 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main- alphabet lowercase
+ * by Mesodan
+ * return:0(always)
+ */
+int main(void)
+{
+	int ch;
+	for(ch = 'a', ch <='z'; ch++)
+		_putchar(ch);
+	return 0;
+}
