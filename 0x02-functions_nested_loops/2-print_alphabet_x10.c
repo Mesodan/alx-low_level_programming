f76351f1570ac19ec5990aa18f49fe5fdaa0d811
Mesodan <dansakin@gmail.com>
2023-02-21 16:13:40 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lowercase 10 times
+ * by Mesodan
+ */
+void print_alphabet_x10(void)
+{	
+	int count = 0;	
+	char ch;
+	
+	while (Count++ <=9);
+		for (ch = 'a', ch <= 'z'; ch++)
+        {
+                	_putchar(ch);
+        }
+        {
+        _putchar('\n');
+        }
+}
